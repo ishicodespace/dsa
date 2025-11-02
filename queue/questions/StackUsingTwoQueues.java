d29543eb1c18171b908dd8b2d0d312e42e8394d0
Ishika Agarwal <ishikaagarwal2211@gmail.com>
2025-11-02 23:42:06 +0530

    queue

--- queue/questions/StackUsingTwoQueues.java
+++ queue/questions/StackUsingTwoQueues.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class StackUsingTwoQueues {
-    public static class TwoQueueStack{
+    public static class TwoQueueStack {
         static Queue<Integer> q1 = new LinkedList<>();
         static Queue<Integer> q2 = new LinkedList<>();
 
@@ -16,7 +16,7 @@ public class StackUsingTwoQueues {
         //enqueue -> O(1)
         public static void add(int data) {
             //push into non-empty queue
-            if(!q1.isEmpty()) {
+            if (!q1.isEmpty()) {
                 q1.add(data);
             } else {
                 q2.add(data);
@@ -25,28 +25,28 @@ public class StackUsingTwoQueues {
 
         //dequeue -> O(n)
         public static int remove() {
-            if(q1.isEmpty() && q2.isEmpty()) {
+            if (q1.isEmpty() && q2.isEmpty()) {
                 System.out.println("Stack is empty");
                 return -1;
             }
             int top = -1;
 
             //case 1: if q1 is non-empty
-            if(!q1.isEmpty()) {
-                while(!q1.isEmpty()){
-                    top=q1.remove();
-                    if(q1.isEmpty()) {
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    if (q1.isEmpty()) {
                         break;
                     }
                     q2.add(top);
                 }
-        }
+            }
 
-        //case 2: if q2 is non-empty
+            //case 2: if q2 is non-empty
             else {
-                while(!q2.isEmpty()){
-                    top=q2.remove();
-                    if(q2.isEmpty()) {
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty()) {
                         break;
                     }
                     q1.add(top);
@@ -57,20 +57,41 @@ public class StackUsingTwoQueues {
 
         //peek -> O(n)
         public static int peek() {
-            if(q1.isEmpty() && q2.isEmpty()) {
+            if (q1.isEmpty() && q2.isEmpty()) {
                 System.out.println("Stack is empty");
                 return -1;
             }
             int top = -1;
 
             //case 1: if q1 is non-empty
-            if(!q1.isEmpty()) {
-                while(!q1.isEmpty()){
-                    top=q1.remove();
-                    q2.add(top);
-                }
-                
+            //case 1: if q1 is non-empty
+        if(!q1.isEmpty())
+
+        {
+            while (!q1.isEmpty()) {
+                top = q1.remove();
+                q2.add(top);
+            }
         }
-    public static void main(String[] args) {
+        //case 2: if q2 is non-empty
+        else
+        {
+            while (!q2.isEmpty()) {
+                top = q2.remove();
+                q1.add(top);
+            }
+        }
+        return top;
+    }
+        public static void main(String[] args) {
+            TwoQueueStack s = new TwoQueueStack();
+            s.add(1);
+            s.add(2);
+            s.add(3);
+            s.add(4);
+                System.out.println(s.peek());
+                System.out.println(s.remove());
+              
+            }
     }
 }
