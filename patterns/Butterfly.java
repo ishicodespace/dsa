5489926d4a465b0332313b0ebe93be3a536c0c84
ishicodespace <ishikaagarwal2211@gmail.com>
2026-05-11 23:53:37 +0530

    butterfly pattern

--- patterns/Butterfly.java
+++ patterns/Butterfly.java
@@ -1,31 +1,30 @@
 package patterns;
+
 public class Butterfly {
     public static void main(String[] args) {
-        int n=10;
+        int n = 10;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n*2; j++) {
-                if(i>=j||i+j>=(n*2)+1){             
+            for (int j = 1; j <= n * 2; j++) {
+                if (i >= j || i + j >= (n * 2) + 1) {
                     System.out.print("*");
 
-                } 
-                else {
+                } else {
                     System.out.print("-");
                 }
             }
-         
+
             System.out.println();
         }
-        for (int i = n; i >=1 ; i--) {
-            for (int j = 1; j <= n*2; j++) {
-                if(i>=j||i+j>=(n*2)+1){             
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n * 2; j++) {
+                if (i >= j || i + j >= (n * 2) + 1) {
                     System.out.print("*");
 
-                } 
-                else {
+                } else {
                     System.out.print("-");
                 }
             }
-         
+
             System.out.println();
         }
     }
