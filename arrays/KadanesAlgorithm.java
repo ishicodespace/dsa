9013194a59c1918144f5cc9206d42c293c7e0d3a
ishicodespace <ishikaagarwal2211@gmail.com>
2026-05-11 23:52:43 +0530

    arrays

--- arrays/KadanesAlgorithm.java
+++ arrays/KadanesAlgorithm.java
@@ -2,15 +2,15 @@ package arrays;
 
 public class KadanesAlgorithm {
     public static void main(String[] args) {
-        int arr[]={-7,-1,-3,-9,-4,-1,-10,0};
-        int current_sum=0,max_sum=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-current_sum=current_sum+arr[i];
-max_sum=Math.max(max_sum, current_sum);
+        int arr[] = { -7, -1, -3, -9, -4, -1, -10, 0 };
+        int current_sum = 0, max_sum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            current_sum = current_sum + arr[i];
+            max_sum = Math.max(max_sum, current_sum);
 
-if(current_sum<0){
-    current_sum=0;
-}
+            if (current_sum < 0) {
+                current_sum = 0;
+            }
         }
         System.out.println(max_sum);
     }
--- arrays/LonelyNumber.java
+++ arrays/LonelyNumber.java
@@ -4,31 +4,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class LonelyNumber {
-  public static ArrayList<Integer> lonely(int nums[]){
-    ArrayList<Integer>  list=new ArrayList<>();
-    Arrays.sort(nums); //nlogn
-for(int i=0;i<nums.length;i++){
-    if(i==0){
-    if(nums[i+1]!=nums[i]+1 &&nums[i+1]!=nums[i]){
-        list.add(nums[i]);
-    }
-}
-    else if(i==nums.length-1){
-    if(nums[i-1]!=nums[i]-1 &&nums[i-1]!=nums[i]){
-        list.add(nums[i]);
-    }
-    }
-    else{
-        if(nums[i-1]!=nums[i]-1&&nums[i+1]!=nums[i]+1&&nums[i-1]!=nums[i]&&nums[i+1]!=nums[i]){
-            list.add(nums[i]);
+    public static ArrayList<Integer> lonely(int nums[]) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Arrays.sort(nums); // nlogn
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                if (nums[i + 1] != nums[i] + 1 && nums[i + 1] != nums[i]) {
+                    list.add(nums[i]);
+                }
+            } else if (i == nums.length - 1) {
+                if (nums[i - 1] != nums[i] - 1 && nums[i - 1] != nums[i]) {
+                    list.add(nums[i]);
+                }
+            } else {
+                if (nums[i - 1] != nums[i] - 1 && nums[i + 1] != nums[i] + 1 && nums[i - 1] != nums[i]
+                        && nums[i + 1] != nums[i]) {
+                    list.add(nums[i]);
+                }
+            }
+
         }
+        return list;
     }
-    
-}
-return list;
-  } 
-  public static void main(String[] args) {
-    int nums[]={10,6,5,8};
-    System.out.println(lonely(nums));
-  } 
+
+    public static void main(String[] args) {
+        int nums[] = { 10, 6, 5, 8 };
+        System.out.println(lonely(nums));
+    }
 }
